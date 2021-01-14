44ec4d351d269459e409594e10fe9b99d35661a6
Romain Clement <contact@romain-clement.net>
2021-01-14 14:47:07 +0100

    Update about

--- ephemere_box/__about__.py
+++ ephemere_box/__about__.py
@@ -1,7 +1,9 @@
 version = (0, 0, 0)
 __version__ = ".".join(str(v) for v in version)
-__title__ = "ephemere-box"
-__description__ = "Micro-service to handle ephemere boxes"
+__title__ = "Ephemere Box"
+__description__ = (
+    "Store and retrieve end-to-end encrypted and ephemeral messages from Ephemere"
+)
 __author__ = "Romain Clement"
 __author_email__ = "contact@romain-clement.net"
 __url__ = "https://github.com/ephemere-app/ephemere-box"
